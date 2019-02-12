c2fcc9acdbc749327e530e53f81a78256db7366f
deadx <deadx@example.com>
2019-02-12 01:21:02 +0100

    Pierwszy program Hello.Tablica args

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Hello
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string name = args[0];
+
+            Console.WriteLine("Witaj, " + name);
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj, " + item);
+            //}
         }
     }
 }
